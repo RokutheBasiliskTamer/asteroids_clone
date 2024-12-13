2e98d4d6962612f58533e5e6593af96276999ebc
RokutheBasiliskTamer <bduckworth99@gmail.com>
2024-12-12 23:13:04 -0500

    asteroids!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,7 +12,18 @@ def main():
     timer = pygame.time.Clock()
     dt = 0
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    field = AsteroidField()
+    
+    
 
     while(True):
         for event in pygame.event.get():
@@ -19,8 +32,10 @@ def main():
 
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
+        for obj in updateable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         
 
         pygame.display.flip()
